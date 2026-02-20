f3f46e2a3234e405602dfc2d6f18a48809870a13
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2026-02-20 20:00:50 +0000

    start.sh works properly

--- start.sh
+++ start.sh
@@ -6,19 +6,15 @@ pkill -x rust 2>/dev/null || true
 pkill -f "deno.*server" 2>/dev/null || true
 
 cleanup() {
-    kill "$deno_pid" "$ndc_pid" 2>/dev/null || true
-    wait "$deno_pid" "$ndc_pid" 2>/dev/null || true
+    kill "$deno_pid" 2>/dev/null || true
+    wait "$deno_pid" 2>/dev/null || true
 }
 
-trap cleanup INT TERM EXIT
+trap cleanup INT TERM
 
 cd /home/quirinpa/tty.pt/ssr
 /home/quirinpa/.deno/bin/deno run --allow-read --allow-net server.ts &
 deno_pid=$!
-sleep 1
 
 cd /home/quirinpa/tty.pt
-/home/quirinpa/ndc/bin/ndc -C /home/quirinpa/tty.pt -p 8080 &
-ndc_pid=$!
-
-wait "$ndc_pid" "$deno_pid"
+exec /home/quirinpa/ndc/bin/ndc -C /home/quirinpa/tty.pt -p 8080 -d
